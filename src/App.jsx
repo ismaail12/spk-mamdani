c1ae8b29bfe4b27bf3f1bba3dcf46e3a3255318b
ismaail12 <ismairacle@gmail.com>
2022-12-19 12:24:46 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { resiko } from './fuzzy/tsukamoto'
 import { Container } from '@mui/system'
 import { TextField, Grid, Button, Typography, Autocomplete } from '@mui/material';
 
-const options = ['Tidak', 'Ada'];
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Content from './Content';
 
+const theme = createTheme({
+
+});
 
 function App() {
-  const [tekananDarah, setTekananDarah] = useState(0);
-  const [gulaDarah, setGulaDarah] = useState(0);
-  const [kolesterol, setKolesterol] = useState(0);
-  const [beratBadan, setBeratBadan] = useState(0);
-  const [tinggiBadan, setTinggBadan] = useState(0);
-  const [bmi, setBMI] = useState(0);
-  const [riwayat, setRiwayat] = useState(options[0]);
-  const [inputRiwayat, setInputRiwayat] = useState(0);
-  const [hasil, setHasil] = useState(0);
-  const [tingkatResiko, setTingkatResiko] = useState('');
-
-
-  const tingkatResikoHandler = () => {
-    if (hasil < 0.4) {
-      setTingkatResiko('Kecil')
-    } else if (hasil > 0.4 && hasil < 0.9) {
-      setTingkatResiko('Sedang')
-    } else if (hasil > 0.9) {
-      setTingkatResiko('Besar')
-    }
-  }
-
-  const bmiHandler = () => setBMI(beratBadan / ((tinggiBadan / 100) ** 2))
-
-  const onClickHandler = () => {
-    bmiHandler();
-    setHasil(resiko(tekananDarah, gulaDarah, kolesterol, bmi, inputRiwayat).toFixed(2));
-    tingkatResikoHandler();
-
-  }
-
 
   return (
-    <Container>
-      <Grid marginTop={8} marginBottom={4} container direction='column'
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}>
-        <Grid item>
-          <Typography variant="h4" align='center' component="h2">
-            Sistem Diagnosa Penyakit Jantung
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="body1" align='center' component="body">
-            Ismail - 191011400124
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid marginTop={4} marginBottom={4} container direction='row'
-        justifyContent="center"
-        alignItems="flex-start"
-        spacing={2}>
-        <Grid item>
-          <Grid container direction='row' justifyContent="center"
-            alignItems="flex-start" spacing={2}>
-            <Grid item sx={{ width: 150 }}>
-              <TextField id="tekanan-darah" label="Tekanan Darah (mmhg)" type="number" InputLabelProps={{ shrink: true }} variant="outlined" size='small' onChange={(event) => {
-                setTekananDarah(event.target.value)
-              }} />
-            </Grid>
-            <Grid item sx={{ width: 150 }}>
-              <TextField id="gula-darah" label="Gula Darah (mmhg)" type="number" InputLabelProps={{ shrink: true }} variant="outlined" size='small' onChange={(event) => {
-                setGulaDarah(event.target.value)
-              }} />
-            </Grid>
-            <Grid item sx={{ width: 150 }}>
-              <TextField id="kolesterol" label="Kolesterol (mg/dl)" type="number" InputLabelProps={{ shrink: true }} variant="outlined" size='small' onChange={(event) => {
-                setKolesterol(event.target.value)
-              }} />
-            </Grid>
-            <Grid item sx={{ width: 150 }}>
-              <TextField id="tinggi-badan" label="Tinggi Badan (cm)" type="number" InputLabelProps={{ shrink: true }} variant="outlined" size='small' onChange={(event) => {
-                setTinggBadan(event.target.value)
-              }} />
-            </Grid>
-            <Grid item sx={{ width: 150 }}>
-              <TextField id="berat-badan" label="Berat Badan (kg)" type="number" InputLabelProps={{ shrink: true }} variant="outlined" size='small' onChange={(event) => {
-                setBeratBadan(event.target.value)
-              }} />
-            </Grid>
-
-            <Grid item sx={{ width: 150 }}>
-              <Autocomplete
-                value={riwayat}
-                onChange={(event, newValue) => { if (newValue === 'Ada') setInputRiwayat(1) }}
-                id="controllable-states-demo"
-                options={options}
-                sx={{ width: 135 }}
-                renderInput={(params) => <TextField {...params} label="Riwayat" size='small' InputLabelProps={{ shrink: true }} />}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} justifyContent="center" alignItems="center" direction='column'>
-        <Grid item sx={{ width: 120 }}>
-          <Button variant='contained' onClick={onClickHandler}>Hitung</Button>
-        </Grid>
-        <Grid item >
-          <Typography variant="body1" align='center' component="body">
-            Tingkat Resiko : {tingkatResiko}
-          </Typography>
-          <Typography variant="body1" align='center' component="body">
-            Dengan Nilai : {hasil}
-          </Typography>
-        </Grid>
-      </Grid>
-
-    </Container>
-
+    <ThemeProvider theme={theme}>
+      <Content />
+    </ThemeProvider>
   )
 }
 
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,118 @@
-import React from 'react'
+import { Button, Stack, TextField, Container, Typography, Grid, Alert } from '@mui/material';
+import { resiko } from './fuzzy/tsukamoto';
+
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import React, { useState, useEffect } from 'react'
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from '@hookform/error-message';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const options = ['Ada', 'Tidak'];
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 200,
+  bgcolor: 'background.paper',
+  borderRadius: '10px',
+  boxShadow: 24,
+  p: 4,
+  
+};
+
 
 const Content = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const [hasil, setHasil] = useState(0);
+  const [tingkatResiko, setTingkatResiko] = useState(0);
+
+  const handleOpen = () => setOpenModal(true);
+  const handleClose = () => setOpenModal(false);
+
+  const tingkatResikoHandler = () => {
+    if (hasil < 0.4) {
+      setTingkatResiko('Kecil')
+    } else if (hasil > 0.4 && hasil < 0.9) {
+      setTingkatResiko('Sedang')
+    } else if (hasil > 0.9) {
+      setTingkatResiko('Besar')
+    }
+  }
+
+  const onSubmit = data => {
+    const riwayatFinal = riwayat === "Ada" ? 1 : 0
+    const bmi = data.beratBadan / ((data.tinggiBadan / 100) ** 2)
+    setHasil(resiko(data.tekananDarah, data.gulaDarah, data.kolesterol, bmi, riwayatFinal).toFixed(2));
+    handleOpen();
+  };
+  const [riwayat, setRiwayat] = React.useState(options[0]);
+  const [inputRiwayat, setNewRiwayat] = React.useState('');
+  useEffect(() => {
+    tingkatResikoHandler()
+  }), [hasil];
+
   return (
-    <div>Content</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container sx={{ paddingTop: 8, paddingBottom: 8 }}>
+          <Grid container
+            alignItems="center"
+            justifyContent="center" spacing={3}>
+            <Grid item xs={12}><Typography variant="h4" color="initial" textAlign='center'>Sistem Diagnosa Penyakit Jantung</Typography></Grid>
+            <Grid item xs={10}><Typography variant="body2" color="initial" textAlign='center'>Ismail - 191011400124</Typography></Grid>
+            <Grid item xs={10} md={4}>
+              <Stack direction='column' gap={2} alignContent="center">
+                <TextField label='Tekanan Darah (mmhg)' type={'number'} size='small' {...register("tekananDarah", {
+                  required: true
+                })} />
+                { }
+                <TextField label='Gula Darah (mmhg)' type={'number'} size='small' {...register("gulaDarah", { required: true })} />
+                <TextField label='Kolesterol (mg/dl)' type={'number'} size='small' {...register("kolesterol", { required: true })} />
+                <TextField label='Tinggi Badan (cm)' type={'number'} size='small' {...register("tinggiBadan", { required: true })} />
+                <TextField label='Berat Badan (kg)' type={'number'} size='small' {...register("beratBadan", { required: true })} />
+                <Autocomplete
+                  value={riwayat}
+                  onChange={(event, newValue) => {
+                    setRiwayat(newValue);
+                  }}
+                  inputValue={inputRiwayat}
+                  onInputChange={(event, newinputRiwayat) => {
+                    setNewRiwayat(newinputRiwayat);
+                  }}
+                  id="controllable-states-demo"
+                  options={options}
+                  size='small'
+                  renderInput={(params) => <TextField {...params} label="Riwayat Keluarga" />}
+                />
+                <Button type='submit' variant='contained'>
+                  Diagnosa
+                </Button>
+                <Modal
+                  open={openModal}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Typography textAlign={'center'} id="modal-modal-title" variant="h6" component="h2">
+                      Hasil Diagnosa
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                      Tingkat resiko {tingkatResiko} dengan nilai {hasil}
+                    </Typography>
+                  </Box>
+                </Modal>
+              </Stack>
+
+            </Grid>
+          </Grid>
+        </Container>
+
+      </form>
+    </div>
   )
 }
 
